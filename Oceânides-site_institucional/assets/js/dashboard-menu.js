ecf5d0cd7200885c0d1f78970704fc47e9faf659
lean-dro <72211521+lean-dro@users.noreply.github.com>
2023-04-25 15:17:01 -0300

    Correções de cards

--- Oceânides-site_institucional/assets/js/dashboard-menu.js
+++ Oceânides-site_institucional/assets/js/dashboard-menu.js
@@ -1,14 +1,17 @@
 var menuFechado = document.getElementById("menu-fechado");
 var menuAberto = document.getElementById("menu-aberto");
+var conteudosDashboard = document.getElementsByClassName("conteudo").style;
 function abrirFecharMenu() {
   if (menuFechado.style.display == "flex") {
     menuFechado.style.display = "none";
     menuAberto.style.display = "flex";
     conteudo.style.width = "75%";
+  
   } else {
     menuAberto.style.display = "none";
     menuFechado.style.display = "flex";
     conteudo.style.width = "95%";
+    
   }
 }
 
